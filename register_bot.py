c3451b1c2da40921640aaf5bfda68e791d678d4f
snewds <snewds14kmt0a@hotmail.com>
2024-07-30 13:00:00 +0800

    Add files via upload

---
+++ register_bot.py
@@ -0,0 +1,3 @@
+# Facebook bot auto register Facebook注册机 脸书注册机 Facebook账号生成器 脸书账号生成器 Facebook注册工具 Facebook账号批量生成 脸书注册工具 脸书账号批量生成 脸书批量注册脚本
+print("需要脚请联系(付费): https://t.me/constantsas")
+print("If you need feet, please contact (paid): https://t.me/constantsas")
